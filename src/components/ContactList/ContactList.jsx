0090e82e8d143d65f264b7ad74d1025bb42d771f
vladyslavbabenk0 <thevladman17@gmail.com>
2023-08-16 20:43:30 +0200

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,42 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
+import ContactItem from './ContactItem';
 
 const ContactList = ({ contactList, handleDelete }) => {
   return (
     <ul className={css.contactList}>
       {contactList.map(({ id, name, number }) => (
-        <li key={id} className={css.contactItem}>
-          
-          <span>
-            {name}: {number}
-          </span>
-
-          <button
-            className={css.contactItemBtn}
-            type="button"
-            onClick={() => {
-              handleDelete(id);
-            }}  
-          >
-            Delete
-          </button>
-        </li>
+        <ContactItem
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          handleDelete={handleDelete}
+        />
       ))}
     </ul>
   );
 };
 
 ContactList.propTypes = {
-  contactList: PropTypes.arrayOf 
-  (
+  contactList: PropTypes.arrayOf(
     PropTypes.exact({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
-      }).isRequired
-   )
-   .isRequired,
+    }).isRequired
+  ).isRequired,
   handleDelete: PropTypes.func.isRequired,
 };
 
